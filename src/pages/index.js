de8b8696cf67447f2834dde6072619eee7fde4c8
Nicholas Steinbauer <web@obrien.com>
2023-01-18 11:32:44 -0500

    new image and favicons

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,16 @@ export default function Home() {
         <meta name="description" content="Find great things at the Put-in-Bay Picnic Basket" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#ff0000" />
+        <meta name="msapplication-TileColor" content="#ffc40d" />
+        <meta name="theme-color" content="#ffffff" />
+
+
       </Head>
       <div className={styles.mainBack}></div>
 
